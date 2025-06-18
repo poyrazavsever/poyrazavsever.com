73053fa35c3a4a98e2868df22e08c37a8a75b69b
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-18 16:27:36 +0300

    feat: added stick future to navbar

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -3,7 +3,7 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <div className="max-w-5xl mx-auto container min-h-screen flex flex-col">
+    <div className="max-w-5xl mx-auto container min-h-screen flex flex-col relative">
 
       <Navbar />
 
--- components/layout/navbar.jsx
+++ components/layout/navbar.jsx
@@ -2,13 +2,12 @@
 
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 import DropdownMenu from './DropdownMenu';
 import ProfileMenu from './ProfileMenu';
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-white border-b border-neutral-200 py-3 flex justify-between items-center">
+    <nav className="sticky top-0 left-0 z-50 w-full bg-white border-b border-neutral-200 py-3 flex justify-between items-center">
       {/* Logo */}
       <div className="flex items-center gap-2">
         <div className="w-3 h-3 bg-black rounded-sm" />
