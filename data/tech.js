7eb67b95277c57fef46e5232923c08f83208176b
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-18 16:21:24 +0300

    feat: added tech stack data

---
+++ data/tech.js
@@ -0,0 +1,45 @@
+export const tech = [
+    // Frontend
+    { iconName: "html", category: "frontend", title: "HTML5" },
+    { iconName: "css", category: "frontend", title: "CSS3" },
+    { iconName: "sass", category: "frontend", title: "SASS" },
+    { iconName: "tailwind", category: "frontend", title: "Tailwind CSS" },
+    { iconName: "bootstrap", category: "frontend", title: "Bootstrap" },
+    { iconName: "materialui", category: "frontend", title: "Material UI" },
+    { iconName: "react", category: "frontend", title: "React" },
+    { iconName: "nextjs", category: "frontend", title: "Next.js" },
+    { iconName: "vue", category: "frontend", title: "Vue.js" },
+    { iconName: "nuxtjs", category: "frontend", title: "Nuxt.js" },
+    { iconName: "svelte", category: "frontend", title: "Svelte" },
+    { iconName: "redux", category: "frontend", title: "Redux" },
+    { iconName: "pug", category: "frontend", title: "Pug" },
+  
+    // Language
+    { iconName: "js", category: "language", title: "JavaScript" },
+    { iconName: "ts", category: "language", title: "TypeScript" },
+    { iconName: "python", category: "language", title: "Python" },
+  
+    // Backend & Databases
+    { iconName: "nodejs", category: "backend", title: "Node.js" },
+    { iconName: "express", category: "backend", title: "Express.js" },
+    { iconName: "nestjs", category: "backend", title: "NestJS" },
+    { iconName: "fastapi", category: "backend", title: "FastAPI" },
+    { iconName: "firebase", category: "database", title: "Firebase" },
+    { iconName: "supabase", category: "database", title: "Supabase" },
+    { iconName: "sqlite", category: "database", title: "SQLite" },
+    { iconName: "mysql", category: "database", title: "MySQL" },
+    { iconName: "mongodb", category: "database", title: "MongoDB" },
+  
+    // Tools & Platforms
+    { iconName: "figma", category: "tool", title: "Figma" },
+    { iconName: "xd", category: "tool", title: "Adobe XD" },
+    { iconName: "ps", category: "tool", title: "Photoshop" },
+    { iconName: "sketchup", category: "tool", title: "SketchUp" },
+    { iconName: "vercel", category: "platform", title: "Vercel" },
+    { iconName: "git", category: "tool", title: "Git" },
+    { iconName: "gitlab", category: "tool", title: "GitLab" },
+    { iconName: "github", category: "tool", title: "GitHub" },
+    { iconName: "vscode", category: "tool", title: "VS Code" },
+    { iconName: "visualstudio", category: "tool", title: "Visual Studio" },
+  ];
+  
