dbf33c3dd5b737e9311e1a0be0996794daa930b8
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-19 15:06:27 +0300

    feat: added react hot toast

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -1,11 +1,14 @@
 import Navbar from './navbar';
 import Footer from './footer';
+import { Toaster } from 'react-hot-toast';
 
 const Layout = ({ children }) => {
   return (
     <div className="max-w-5xl mx-auto container min-h-screen flex flex-col relative">
 
       <Navbar />
+
+      <Toaster position='top-center'/>
 
       <main className="flex-1">{children}</main>
 
