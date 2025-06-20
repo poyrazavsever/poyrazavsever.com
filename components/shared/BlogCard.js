68e632ba43fac0fee8808fe8eeebf8aade1fa55e
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-20 16:03:06 +0300

    feat: added language future to link

--- components/shared/BlogCard.js
+++ components/shared/BlogCard.js
@@ -5,6 +5,13 @@ import { useTranslation } from 'next-i18next';
 
 const BlogCard = ({ cardImage, title, description, slug }) => {
   const { t } = useTranslation('common');
+
+  // Local storage'dan language çek
+  let lang = 'tr';
+  if (typeof window !== 'undefined') {
+    lang = localStorage.getItem('language') || 'tr';
+  }
+
   return (
     <div className="rounded-xl overflow-hidden border border-neutral-200 dark:border-neutral-700 p-4 w-full max-w-xs">
       <div className="relative w-full h-48 rounded-lg overflow-hidden">
@@ -18,7 +25,7 @@ const BlogCard = ({ cardImage, title, description, slug }) => {
       <div className="mt-4 flex flex-col gap-1 items-start">
         <h3 className="text-neutral-800 dark:text-neutral-200 font-semibold line-clamp-1">{title}</h3>
         <p className="text-sm text-neutral-600 dark:text-neutral-400 mt-1 line-clamp-1">{description}</p>
-        <Link href={`/blog/${slug}`}>
+        <Link href={`/${lang}/blog/${slug}`}>
           <span className="text-sm text-sky-500 dark:text-sky-400 underline hover:text-sky-800 transition-all cursor-pointer mt-2">
             {t('details')}
           </span>
--- components/shared/ProjectCard.js
+++ components/shared/ProjectCard.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { useTranslation } from 'next-i18next';
-const ProjectCard = ({ imageSrc, title, description, slug}) => {
+
+const ProjectCard = ({ imageSrc, title, description, slug }) => {
   const { t } = useTranslation('common');
+
+  // Local storage'dan language çek
+  let lang = 'tr';
+  if (typeof window !== 'undefined') {
+    lang = localStorage.getItem('language') || 'tr';
+  }
+
   return (
     <div className="rounded-xl overflow-hidden border border-neutral-200 dark:border-neutral-700 p-4 w-full md:max-w-xs">
       <div className="relative w-full h-52 md:h-48 rounded-lg overflow-hidden">
@@ -15,7 +23,10 @@ const ProjectCard = ({ imageSrc, title, description, slug}) => {
       <div className="mt-4 flex flex-col gap-1 items-start">
         <h3 className="text-sm text-neutral-800 dark:text-neutral-200 font-semibold">{title}</h3>
         <p className="text-sm text-neutral-600 dark:text-neutral-400 mt-1 line-clamp-4 break-words whitespace-normal">{description}</p>
-        <a href={`/projects/${slug}`} className="text-sm text-sky-500 dark:text-sky-600 underline hover:text-sky-800 transition-all cursor-pointer">
+        <a
+          href={`/${lang}/projects/${slug}`}
+          className="text-sm text-sky-500 dark:text-sky-600 underline hover:text-sky-800 transition-all cursor-pointer"
+        >
           {t('details')}
         </a>
       </div>
