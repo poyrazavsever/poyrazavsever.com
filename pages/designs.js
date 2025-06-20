3ea66a971b89254e16413fefbd98fe6b5eaefce6
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-20 13:33:17 +0300

    feat: done designs page site

--- pages/designs.js
+++ pages/designs.js
@@ -2,9 +2,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { projects } from '@/data/projects';
 import ProjectCard from '@/components/shared/ProjectCard';
+import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const Designs = () => {
+  const { t } = useTranslation('designs');
   const designProjects = projects.filter((project) => project.category === 'design');
 
   return (
@@ -22,12 +24,17 @@ const Designs = () => {
         </div>
 
         <div className="w-full sm:w-1/2 space-y-3 text-left">
-          <h1 className="text-2xl sm:text-3xl font-bold text-neutral-800 dark:text-neutral-100">Tasarımla da İlgileniyorum!</h1>
+          <h1 className="text-2xl sm:text-3xl font-bold text-neutral-800 dark:text-neutral-100">
+            {t('title')}
+          </h1>
           <p className="text-sm text-neutral-600 dark:text-neutral-300 leading-relaxed">
-            UI/UX tasarımı, sadece güzel görünen değil, aynı zamanda sezgisel ve etkili kullanıcı deneyimleri sunan arayüzler oluşturma sürecidir. Farklı projelerde bu alanda fikir üretmeyi ve tasarlamayı seviyorum.
+            {t('description')}
           </p>
-          <Link href="/contact" className="w-full block text-center bg-neutral-600 text-white px-4 py-2 rounded-md text-sm hover:bg-neutral-800 dark:hover:bg-neutral-700 transition cursor-pointer">
-              İletişime Geç
+          <Link
+            href="/contact"
+            className="w-full block text-center bg-neutral-600 text-white px-4 py-2 rounded-md text-sm hover:bg-neutral-800 dark:hover:bg-neutral-700 transition cursor-pointer"
+          >
+            {t('button')}
           </Link>
         </div>
       </div>
@@ -35,7 +42,7 @@ const Designs = () => {
       {/* Design Projects */}
       {designProjects.length > 0 && (
         <div className="max-w-6xl w-full flex flex-col gap-6 mt-16">
-          <h2 className="text-xl font-semibold">UI/UX Projelerim</h2>
+          <h2 className="text-xl font-semibold">{t('sectionTitle')}</h2>
 
           <div className="flex flex-wrap gap-6 justify-center sm:justify-between">
             {designProjects.map((project, idx) => (
@@ -48,7 +55,6 @@ const Designs = () => {
               />
             ))}
           </div>
-
         </div>
       )}
     </div>
@@ -58,7 +64,7 @@ const Designs = () => {
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common', 'layout'])),
+      ...(await serverSideTranslations(locale, ['designs', 'layout'])),
     },
   };
 }
