9036a3cf818e972b56fb691c9fdfbbc252663e5f
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-20 13:34:19 +0300

    feat: done gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -2,23 +2,13 @@
 
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-
-const galleryImages = [
-  {
-    img:'/gallery/1.png',
-    title:"Yapay Zeka Çağında Sürdürülebilirlik, Uluslar Arası Öğrenci Kongresi"
-  },
-  {
-    img:'/gallery/2.png',
-    title:"Yapay Zeka Çağında Sürdürülebilirlik, Uluslar Arası Öğrenci Kongresi"
-  },
-  {
-    img:'/gallery/1.png',
-    title:"Yapay Zeka Çağında Sürdürülebilirlik, Uluslar Arası Öğrenci Kongresi"
-  },
-];
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const Gallery = () => {
+  const { t } = useTranslation('gallery');
+  const galleryImages = t('images', { returnObjects: true });
+
   const [isOpen, setIsOpen] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -39,10 +29,10 @@ const Gallery = () => {
     <>
       {/* Grid Gallery */}
       <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4 my-8 sm:my-16">
-        {galleryImages.map((src, idx) => (
+        {galleryImages.map((item, idx) => (
           <div key={idx} onClick={() => openModal(idx)} className="cursor-pointer">
             <img
-              src={src.img}
+              src={item.img}
               alt={`Gallery image ${idx + 1}`}
               className="object-cover h-64 w-64 rounded-md"
             />
@@ -60,7 +50,7 @@ const Gallery = () => {
             exit={{ opacity: 0 }}
             onClick={() => setIsOpen(false)}
           >
-            <div className='flex flex-col items-center gap-4 px-2'>
+            <div className="flex flex-col items-center gap-4 px-2">
               <motion.img
                 src={galleryImages[currentIndex].img}
                 alt={`Modal image ${currentIndex + 1}`}
@@ -68,13 +58,31 @@ const Gallery = () => {
                 initial={{ scale: 0.95 }}
                 animate={{ scale: 1 }}
                 exit={{ scale: 0.95 }}
-                onClick={(e) => e.stopPropagation()} // Modal dışında tıklama
+                onClick={(e) => e.stopPropagation()}
               />
-              <span className='text-white text-center'>{galleryImages[currentIndex].title}</span>
+              <span className="text-white text-center">
+                {galleryImages[currentIndex].title}
+              </span>
             </div>
             {/* Navigation */}
-            <div className="absolute left-4 text-white text-4xl cursor-pointer" onClick={(e) => { e.stopPropagation(); prevImage(); }}>{'‹'}</div>
-            <div className="absolute right-4 text-white text-4xl cursor-pointer" onClick={(e) => { e.stopPropagation(); nextImage(); }}>{'›'}</div>
+            <div
+              className="absolute left-4 text-white text-4xl cursor-pointer"
+              onClick={(e) => {
+                e.stopPropagation();
+                prevImage();
+              }}
+            >
+              ‹
+            </div>
+            <div
+              className="absolute right-4 text-white text-4xl cursor-pointer"
+              onClick={(e) => {
+                e.stopPropagation();
+                nextImage();
+              }}
+            >
+              ›
+            </div>
           </motion.div>
         )}
       </AnimatePresence>
@@ -83,3 +91,11 @@ const Gallery = () => {
 };
 
 export default Gallery;
+
+export const getStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['gallery', 'layout'])),
+    },
+  };
+};
