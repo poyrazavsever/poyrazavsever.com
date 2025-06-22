a8e7c2e5b40239e475833a242fca6a380488a7b1
Poyraz <poyrazavsever@hotmail.com>
2025-06-22 09:42:57 +0300

    feat: deleted loading screen

--- pages/_app.js
+++ pages/_app.js
@@ -2,34 +2,13 @@
 import { appWithTranslation } from 'next-i18next';
 import Layout from '@/components/layout/layout';
 import '../styles/globals.css';
-import { useState, useEffect } from 'react';
-import LoadingScreen from '@/components/LoadingScreen';
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Sayfa ilk yüklendiğinde loading'i true yap
-    setLoading(true);
-    
-    // 3 saniye sonra loading'i false yap
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
-    <>
-      {loading ? (
-        <LoadingScreen />
-      ) : (
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      )}
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
