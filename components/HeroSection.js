e5e9e96196311d20b6b25f61498de57fc06dcd23
poyrazavsever <poyrazavsever@hotmail.com>
2025-07-02 10:43:35 +0300

    feat: updated colors hero section page site

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -3,29 +3,34 @@ import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
 
 const HeroSection = () => {
-
   const {t} = useTranslation('common')
 
   return (
     <div className="my-4 md:my-24 sm:p-0 flex flex-col md:flex-row gap-8 sm:gap-16 items-start md:items-center justify-between max-w-6xl w-full">
-        <img src="/hero/1.png" alt="hero section 1" className='rounded-xl w-full'/>
+        <img 
+          src="/hero/1.png" 
+          alt="hero section 1" 
+          className='rounded-xl w-full'
+        />
 
-        {/* Sağdaki Metin Alanı */}
-        <div className="text-left space-y-3">
-          <h1 className="text-2xl sm:text-3xl font-bold text-neutral-800 dark:text-neutral-100">
+        <div className="text-left space-y-4">
+          <h1 className="text-2xl sm:text-3xl font-bold text-blue-500 dark:text-blue-300">
             {t('hello')}
           </h1>
-          <p className="text-sm text-neutral-600 dark:text-neutral-300 leading-relaxed">
+          <p className="text-sm text-blue-950/70 dark:text-blue-100 leading-relaxed">
             {t('welcome')}
           </p>
 
-          <Link href="/contact" className="w-full block text-center bg-neutral-600 text-white px-4 py-2 rounded-md text-sm hover:bg-neutral-800 dark:hover:bg-neutral-700 transition cursor-pointer">
+          <Link 
+            href="/contact" 
+            className="w-full block text-center bg-blue-600 text-white px-6 py-2.5 rounded-lg text-sm 
+            hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-800 
+            transition-colors duration-200 border border-blue-400 dark:border-blue-600">
             {t('contact')}
           </Link>
         </div>
-      </div>
+    </div>
   )
 }
-
 
 export default HeroSection
